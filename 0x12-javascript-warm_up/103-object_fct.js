44918c98dbd4e265050f60fe546c9d47c52dd174
steve <austines811@gmail.com>
2022-11-28 23:23:39 +0300

    update to use instance

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,28 @@
 #!/usr/bin/node
+// const myObject = {
+//   type: 'object',
+//   value: 12
+// };
+// console.log(myObject);
+// // increment function that adds 1 to the value field of the object
+
+// myObject.incr = function incr () {
+//   myObject.value++;
+// };
+// myObject.incr();
+// console.log(myObject);
+// myObject.incr();
+// console.log(myObject);
+// myObject.incr();
+// console.log(myObject);
+
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-// increment function that adds 1 to the value field of the object
-
-myObject.incr = function incr () {
-  myObject.value++;
+myObject.incr = function () {
+  this.value++;
 };
 myObject.incr();
 console.log(myObject);
