adb56e6585d4faccd1a370319fd83ddb0b79dcf0
steve <austines811@gmail.com>
2022-11-28 14:52:47 +0300

    removes erranious new line

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,4 +4,4 @@ const lang1 = 'C is fun';
 const lang2 = 'Python is cool';
 const lang3 = 'JavaScript is amazing';
 
-console.log(`${lang1}\n${lang2}\n${lang3}\n`);
+console.log(`${lang1}\n${lang2}\n${lang3}`);
